fa665d8b8d437f4143bd5e908b2fc0f6ff010318
david <github@nonswag.dev>
2025-09-05 18:01:45 +0200

    Remove `getAsString` method from `ValueTag` to prevent unintended side effects

--- src/main/java/net/thenextlvl/nbt/tag/ValueTag.java
+++ src/main/java/net/thenextlvl/nbt/tag/ValueTag.java
@@ -27,11 +27,6 @@ public abstract class ValueTag<T> implements Tag {
     }
 
     @Override
-    public String getAsString() {
-        return value.toString();
-    }
-
-    @Override
     public boolean equals(Object object) {
         if (this == object) return true;
         if (object == null || getClass() != object.getClass()) return false;
