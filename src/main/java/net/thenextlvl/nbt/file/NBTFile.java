2423c0363f46c06e687d79480b3b5cedff8a8ca3
david <mail@nonswag.dev>
2025-11-30 20:06:10 +0100

    Refactor NBTFile to use java.nio.file and remove FileIO

--- src/main/java/net/thenextlvl/nbt/file/NBTFile.java
+++ src/main/java/net/thenextlvl/nbt/file/NBTFile.java
@@ -1,7 +1,5 @@
 package net.thenextlvl.nbt.file;
 
-import core.file.FileIO;
-import core.io.IO;
 import net.thenextlvl.nbt.NBTInputStream;
 import net.thenextlvl.nbt.NBTOutputStream;
 import net.thenextlvl.nbt.tag.CompoundTag;
@@ -10,51 +8,87 @@ import org.jspecify.annotations.Nullable;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.attribute.FileAttribute;
 import java.util.Objects;
 
-import static java.nio.file.StandardOpenOption.*;
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.READ;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+import static java.nio.file.StandardOpenOption.WRITE;
 
 /**
  * Represents an NBT (Named Binary Tag) file that can be read from and written to.
- * This class extends the {@link FileIO} class, allowing for operations specific to NBT files.
  *
  * @param <R> the type of the root object, which extends {@link CompoundTag}
  */
-public class NBTFile<R extends CompoundTag> extends FileIO<R> {
+public class NBTFile<R extends CompoundTag> {
     /**
      * The name of the root tag in an NBT (Named Binary Tag) file.
      * This variable can be null if no root name is specified.
      */
     private @Nullable String rootName;
 
+    private final Path file;
+    private final Charset charset;
+
+    private R root;
+    private boolean loaded;
+
     /**
      * Construct a new NBTFile providing a file, charset and default root object
      *
-     * @param io      the file to read from and write to
+     * @param file    the file to read from and write to
      * @param charset the charset to use for read and write operations
      * @param root    the default root object
      */
-    public NBTFile(@NonNull IO io, @NonNull Charset charset, R root) {
-        super(io, charset, root);
+    public NBTFile(Path file, Charset charset, R root) {
+        this.file = file;
+        this.root = root;
+        this.charset = charset;
     }
 
     /**
      * Construct a new NBTFile providing a file and default root object
      *
-     * @param io   the file to read from and write to
+     * @param file the file to read from and write to
      * @param root the default root object
      */
-    public NBTFile(@NonNull IO io, R root) {
-        super(io, root);
+    public NBTFile(Path file, R root) {
+        this(file, StandardCharsets.UTF_8, root);
     }
 
-    @Override
+    public R getRoot() {
+        if (loaded) return root;
+        loaded = true;
+        return root = load();
+    }
+
+    private NBTFile<R> setRoot(R root) {
+        this.loaded = true;
+        this.root = root;
+        return this;
+    }
+
+    public NBTFile<R> reload() {
+        return setRoot(load());
+    }
+
+    public NBTFile<R> saveIfAbsent(FileAttribute<?>... attributes) {
+        return Files.isRegularFile(file) ? this : save(attributes);
+    }
+
+    public Charset getCharset() {
+        return charset;
+    }
+
     @SuppressWarnings("unchecked")
     protected R load() {
-        if (!getIO().exists()) return getRoot();
+        if (!Files.isRegularFile(file)) return getRoot();
         try (var inputStream = new NBTInputStream(
-                getIO().inputStream(READ),
+                Files.newInputStream(file, READ),
                 getCharset()
         )) {
             var entry = inputStream.readNamedTag();
@@ -65,13 +99,12 @@ public class NBTFile<R extends CompoundTag> extends FileIO<R> {
         }
     }
 
-    @Override
-    public @NonNull FileIO<R> save(@NonNull FileAttribute<?>... attributes) {
+    public @NonNull NBTFile<R> save(@NonNull FileAttribute<?>... attributes) {
         try {
             var root = getRoot();
-            getIO().createParents(attributes);
+            Files.createDirectories(file.getParent());
             try (var outputStream = new NBTOutputStream(
-                    getIO().outputStream(WRITE, CREATE, TRUNCATE_EXISTING),
+                    Files.newOutputStream(file, WRITE, CREATE, TRUNCATE_EXISTING),
                     getCharset()
             )) {
                 outputStream.writeTag(getRootName(), root);
@@ -93,20 +126,21 @@ public class NBTFile<R extends CompoundTag> extends FileIO<R> {
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
         NBTFile<?> nbtFile = (NBTFile<?>) o;
-        return Objects.equals(rootName, nbtFile.rootName);
+        return Objects.equals(rootName, nbtFile.rootName) && Objects.equals(file, nbtFile.file) && Objects.equals(charset, nbtFile.charset);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), rootName);
+        return Objects.hash(rootName, file, charset);
     }
 
     @Override
     public String toString() {
         return "NBTFile{" +
-               "rootName='" + rootName + '\'' +
-               "} " + super.toString();
+                "rootName='" + rootName + '\'' +
+                ", file=" + file +
+                ", charset=" + charset +
+                '}';
     }
 }
