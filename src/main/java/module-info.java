cf005d72b675109f9f86cbbbf326d5aa4432e1d7
david <mail@nonswag.dev>
2025-12-09 20:28:47 +0100

    Update module-info to reorganize exports and requires

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,12 +2,11 @@ import org.jspecify.annotations.NullMarked;
 
 @NullMarked
 module net.thenextlvl.nbt {
+    exports net.thenextlvl.nbt.serialization.adapter;
+    exports net.thenextlvl.nbt.serialization;
+    exports net.thenextlvl.nbt.tag;
+    exports net.thenextlvl.nbt;
+
     requires static org.jetbrains.annotations;
     requires static org.jspecify;
-
-    exports net.thenextlvl.nbt;
-    exports net.thenextlvl.nbt.file;
-    exports net.thenextlvl.nbt.serialization;
-    exports net.thenextlvl.nbt.serialization.adapter;
-    exports net.thenextlvl.nbt.tag;
 }
