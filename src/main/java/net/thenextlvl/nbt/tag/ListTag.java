f719b15d3082272ad11784f04832d17cf50db0c4
david <mail@nonswag.dev>
2025-12-21 04:59:48 +0100

    Fix ListTag empty method and contentType throws

--- src/main/java/net/thenextlvl/nbt/tag/ListTag.java
+++ src/main/java/net/thenextlvl/nbt/tag/ListTag.java
@@ -96,12 +96,10 @@ public sealed interface ListTag<T extends Tag> extends ValueTag<@Unmodifiable Li
      * @param <T>           the type of tags contained in the list
      * @param contentTypeId the byte ID representing the content type of the tag
      * @return a new empty {@link ListTag} with the given content type ID
-     * @throws IllegalArgumentException if the content type ID is EscapeTag
      * @since 4.0.0
      */
     @Contract(value = "_ -> new", pure = true)
-    static <T extends Tag> ListTag<T> empty(byte contentTypeId) throws IllegalArgumentException {
-        if (contentTypeId == EscapeTag.ID) throw new IllegalArgumentException("Content type cannot be EscapeTag");
+    static <T extends Tag> ListTag<T> empty(byte contentTypeId) {
         return new ListTagImpl<>(contentTypeId);
     }
 
@@ -242,11 +240,10 @@ public sealed interface ListTag<T extends Tag> extends ValueTag<@Unmodifiable Li
          *
          * @param contentTypeId the byte ID representing the content type of the tag
          * @return the builder instance, allowing for method chaining
-         * @throws IllegalStateException    if the content type ID is already set and values have been added
-         * @throws IllegalArgumentException if the content type ID is EscapeTag
+         * @throws IllegalStateException if the content type ID is already set and values have been added
          */
         @Contract(value = "_ -> this", mutates = "this")
-        Builder<T> contentType(byte contentTypeId) throws IllegalStateException, IllegalArgumentException;
+        Builder<T> contentType(byte contentTypeId) throws IllegalStateException;
 
         /**
          * Adds a tag value to the builder.
